e2d43362cb3890ef30e08bb2f41a348a8b60a1e4
TCC <78140169+67762961@users.noreply.github.com>
2024-10-12 15:28:50 +0800

    feat:新加入运行日志记录功能

--- main.py
+++ main.py
@@ -9,13 +9,31 @@ import sys
 import pyautogui
 import win32process
 import config
+import os
 import subprocess
+import datetime
 from Task_LogIn import LogIn
 from Task_SignIn import MainTask_Signin
 from Task_Fengmo import MainTask_Fengmo
+from contextlib import redirect_stdout
 
 # 全局变量 窗口句柄列表
 hwnds = []
+
+
+# 创建了一个 Tee 类，用于同时将输出写入文件和控制台
+class Tee:
+    def __init__(self, *files):
+        self.files = files
+
+    def write(self, message):
+        for file in self.files:
+            file.write(message)
+            file.flush()
+
+    def flush(self):
+        for file in self.files:
+            file.flush()
 
 
 def Init():
@@ -163,7 +181,30 @@ def Fengmo_zhishi():
 
 
 def Full_operation():
-    Main_Login_2()
-    Sign_In()
-    Sis_Foster()
-    Fengmo_zhishi()
+    # 获取当前日期和时间，用于生成唯一的文件名
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+    log_dir = os.path.join("logs", now.strftime("%Y-%m-%d"))
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)  # 创建文件夹
+
+    log_file_path = os.path.join(log_dir, f"console_output_{timestamp}.log")  # 添加时间戳
+
+    # 打开日志文件
+    with open(log_file_path, "w", encoding="utf-8") as log_file:
+        # 使用 Tee 类重定向输出
+        original_stdout = sys.stdout  # 保存原始 stdout
+        sys.stdout = Tee(original_stdout, log_file)  # 重定向
+
+        try:
+            print()
+            print("MAIN- ~~~~ 完整运行流程开始 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            Main_Login_2()
+            Sign_In()
+            Sis_Foster()
+            Fengmo_zhishi()
+            print("MAIN- ~~~~ 完整运行流程结束 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+        finally:
+            sys.stdout = original_stdout  # 恢复原始 stdout

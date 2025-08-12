8a894307cc62c679a5d26924fc738891ec0464c2
TCC <78140169+67762961@users.noreply.github.com>
2025-08-12 20:38:26 +0800

    fix:结界任务顺序更换为先经验酒壶 再体力食盒

--- Task_Jiejieyangcheng.py
+++ Task_Jiejieyangcheng.py
@@ -85,12 +85,12 @@ def Jiejieyangcheng(current_state, Hwnd):
 
                 flag_yucheng = Yucheng("结界界面", Hwnd)
 
-                # 领取体力食盒
-                Tilishihe(Hwnd)
-
                 # 经验酒壶 领取后育成
                 if Jinyanjiuhu(Hwnd):
                     flag_yucheng = Yucheng("结界界面", Hwnd)
+
+                # 领取体力食盒
+                Tilishihe(Hwnd)
 
                 if flag_Jiejieka:
                     flag_Jiejieka = Jiejieka(Hwnd)
